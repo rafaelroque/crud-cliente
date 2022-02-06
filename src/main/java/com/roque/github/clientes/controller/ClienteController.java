6ea3b353997d79d4314775b424113ab7ca2bb9fc
Rafael Roque Viana <rafa834@gmail.com>
2022-02-06 20:27:56 -0300

    Find by id

--- src/main/java/com/roque/github/clientes/controller/ClienteController.java
+++ src/main/java/com/roque/github/clientes/controller/ClienteController.java
@@ -1,11 +1,13 @@
 package com.roque.github.clientes.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,15 +19,33 @@ import com.roque.github.clientes.service.ClienteService;
 @RestController
 @RequestMapping("api/v1/clientes")
 public class ClienteController {
-	
+
 	@Autowired
 	private ClienteService service;
-	
+
 	@GetMapping("/todos")
 	public ResponseEntity<List<Cliente>> todos () {
 		return new ResponseEntity<>(service.buscarTodos(),HttpStatus.OK);
 	}
-	
+
+	@GetMapping("/{id}")
+	public ResponseEntity<Cliente> buscarPorId(@PathVariable Integer id) {
+
+		try {
+
+			Optional<Cliente> cliente = service.buscarPorId(id);
+			if(cliente.isPresent()) {
+				return new ResponseEntity<Cliente>(cliente.get(),HttpStatus.OK);
+			}else {
+				return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+			}
+
+		}catch(IllegalStateException e) {
+			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+		}
+
+	}
+
 	@PostMapping("/novo")
 	public ResponseEntity<Cliente> salvar(@RequestBody Cliente cliente){
 		service.salvar(cliente);
--- src/main/java/com/roque/github/clientes/service/ClienteService.java
+++ src/main/java/com/roque/github/clientes/service/ClienteService.java
@@ -1,6 +1,7 @@
 package com.roque.github.clientes.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -16,6 +17,14 @@ public class ClienteService {
 
 	public List<Cliente> buscarTodos(){
 		return repository.findAll();
+	}
+
+	public Optional<Cliente> buscarPorId(Integer id) throws IllegalStateException{
+		if(id==null) {
+			throw new IllegalStateException("Informe o id");
+
+		}
+		return repository.findById(id);
 	}
 
 	public void salvar(Cliente cliente) {
